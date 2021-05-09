0a0f4fd314566a5bbe9a483c91b6f3c4b0367e56
ale3729 <alexandre.ufu@gmail.com>
2021-05-09 17:43:09 -0300

    icluido a função de tela de autenticação

--- core/views.py
+++ core/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
 
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def login_submit(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request,usuario)
+            return redirect('/')
+        else:
+            messages.error(request,'Usuário ou senha inválidos')
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos (request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
